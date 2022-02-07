869218a3341a647c58be4be5c11355e88d135c03
suepiiin <75456594+suepiiin@users.noreply.github.com>
2022-02-07 19:15:51 +0900

    Add files via upload

---
+++ download_5fps.py
@@ -0,0 +1,13 @@
+import os
+
+base_url = 'https://fb-baas-f32eacb9-8abb-11eb-b2b8-4857dd089e15.s3.amazonaws.com/InterHand2.6M/InterHand2.6M.images.5.fps.v1.0/'
+
+for part1 in ('a', 'b'):
+    for part2 in ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'):
+        if part1 == 'b' and part2 == 's':
+            break
+        os.system('wget ' + base_url + 'InterHand2.6M.images.5.fps.v1.0.tar.part' + part1 + part2)
+
+os.system('wget ' + base_url + 'InterHand2.6M.images.5.fps.v1.0.tar.CHECKSUM')
+os.system('wget ' + base_url + 'unzip.sh')
+os.system('wget ' + base_url + 'verify_download.py')
